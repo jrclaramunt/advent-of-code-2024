3b6fdde3049249067c297805ad2c8ba0c45472cf
J. Ramon Claramunt <plokoonmaster@gmail.com>
2024-12-06 23:04:26 +0100

    Added day 5 part 1 solution

---
+++ solution/day5.py
@@ -0,0 +1,56 @@
+import re
+from collections import defaultdict
+
+from utils.base import Day
+
+
+class Day5(Day):
+    def __init__(self, args):
+        self.rules = defaultdict(list)
+        self.updates = []
+
+        split_index = args[0].index('\n')
+        rules = args[0][0:split_index]
+        updates = args[0][split_index+ 1 :]
+
+        for rule in rules:
+            pages = rule.strip().split('|')
+            self.rules[pages[0]].append(pages[1])
+
+        for update in updates:
+            self.updates.append(update.strip().split(','))
+
+
+    def part1(self):
+        return self.valid_updates_calculation(self.updates)
+
+    def part2(self):
+        wrong_updates = []
+
+        for update in self.updates:
+            add = False
+            for i in range(len(update) - 1):
+                if update[i + 1] not in self.rules[update[i]]:
+                    tmp = update[i + 1]
+                    update[i + 1] = update[i]
+                    update[i] = tmp
+                    add = True
+                    break
+            if add:
+                wrong_updates.append(update)
+
+        return self.valid_updates_calculation(wrong_updates)
+
+    def valid_updates_calculation(self, updates):
+        total = 0
+
+        for update in updates:
+            add = True
+            for i in range(len(update) - 1):
+                if update[i + 1] not in self.rules[update[i]]:
+                    add = False
+                    break
+            if add:
+                total += int(update[int(len(update) / 2)])
+
+        return total # 69148 too high
